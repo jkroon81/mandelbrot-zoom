bd2c36db38da6be585e4ea872ba805771fed2ff3
Jacob Kroon <jacob.kroon@gmail.com>
2017-08-01 14:49:37 +0200

    More WIP

--- mandelbrot-zoom.c
+++ mandelbrot-zoom.c
@@ -9,17 +9,23 @@ typedef struct {
 	double a, b;
 } Complex;
 
-static double xmin, xmax, ymin, ymax;
+typedef struct {
+	double xmin;
+	double xmax;
+	double ymin;
+	double ymax;
+} RenderArea;
+
 static int width, height;
 
-static Uint8 eval_point(int x, int y)
+static Uint8 eval_point(RenderArea *a, int x, int y)
 {
 	double pas = 0, pbs = 0;
 	Complex p = { 0, 0 }, t = { 0, 0}, c;
 	Uint8 i = 0;
 
-	c.a = (double) x / width  * (xmax - xmin) + xmin;
-	c.b = (double) y / height * (ymax - ymin) + ymin;
+	c.a = (double) x / width  * (a->xmax - a->xmin) + a->xmin;
+	c.b = (double) y / height * (a->ymax - a->ymin) + a->ymin;
 	do {
 		t = p;
 		p.a = pas - pbs + c.a;
@@ -32,7 +38,7 @@ static Uint8 eval_point(int x, int y)
 	return i;
 }
 
-static void mandelbrot_render(Uint8 *data, int pitch)
+static void mandelbrot_render(RenderArea *a, Uint8 *data, int pitch)
 {
 	int x, y;
 	Uint32 *p;
@@ -40,7 +46,7 @@ static void mandelbrot_render(Uint8 *data, int pitch)
 #pragma omp parallel for private(x,y) shared(data, pitch, p)
 	for (x = 0; x < width; x++)
 		for (y = 0; y < height; y++) {
-			Uint8 val = eval_point(x, y);
+			Uint8 val = eval_point(a, x, y);
 			p = (Uint32*)(data + pitch * y + x * 4);
 			*p = val | val << 8 | val << 16;
 		}
@@ -67,32 +73,37 @@ static Uint32 render_cb(Uint32 interval, void *param)
 }
 
 static SDL_sem *render_sem;
+static Uint8 *frame, *frame_a, *frame_b;
+static RenderArea a_thread;
+static int pitch;
 
 static int render_thread(void *param)
 {
 	while(1) {
 		SDL_SemWait(render_sem);
-		printf("Rendering\n");
+		Uint8 *dst = frame == frame_a ? frame_b : frame_a;
+		mandelbrot_render(&a_thread, dst, pitch);
+		printf("Render done\n");
 	}
 	return 0;
 }
 
 int main(void)
 {
-	int px = 0, py = 0, pitch;
+	int px = 0, py = 0;
 	double zoom = 1.0, zoom_mul = 1.0, p2x, p2y;
 	char title[256];
 	SDL_Event event;
 	SDL_Renderer *rdr;
 	SDL_Texture *txt;
-	Uint8 *frame;
 	Uint32 mstate;
 	SDL_TimerID render_tid = 0;
-
-	xmin = -1.0;
-	xmax = 1.0;
-	ymin = -1.0;
-	ymax = 1.0;
+	RenderArea a = {
+		.xmin = -1.0,
+		.xmax = 1.0,
+		.ymin = -1.0,
+		.ymax = 1.0
+	};
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		error("SDL : %s\n", SDL_GetError());
@@ -120,10 +131,14 @@ int main(void)
 	if (txt == NULL)
 		error("SDL: %s\n", SDL_GetError());
 	pitch = width * 4;
-	frame = malloc(height * pitch);
-	if (frame == NULL)
-		error("Error allocating frame\n");
-	mandelbrot_render(frame, pitch);
+	frame_a = malloc(height * pitch);
+	if (frame_a == NULL)
+		error("Error allocating frame a\n");
+	frame_b = malloc(height * pitch);
+	if (frame_b == NULL)
+		error("Error allocating frame b\n");
+	frame = frame_a;
+	mandelbrot_render(&a, frame, pitch);
 	SDL_UpdateTexture(txt, NULL, frame, pitch);
 	SDL_RenderClear(rdr);
 	SDL_RenderCopy(rdr, txt, NULL, NULL);
@@ -164,16 +179,16 @@ int main(void)
 			break;
 		case SDL_USEREVENT:
 			sprintf(title, TITLE " - Zoom : %.2f",
-			        (2.0 / zoom / (xmax - xmin)));
+			        (2.0 / zoom / (a.xmax - a.xmin)));
 			SDL_SetWindowTitle(_screen, title);
 			zoom *= zoom_mul;
-			p2x = (xmax - xmin) / width * px + xmin;
-			p2y = (ymax - ymin) / height * py + ymin;
-			xmin = p2x - zoom * (p2x - xmin);
-			xmax = p2x - zoom * (p2x - xmax);
-			ymin = p2y - zoom * (p2y - ymin);
-			ymax = p2y - zoom * (p2y - ymax);
-			mandelbrot_render(frame, pitch);
+			p2x = (a.xmax - a.xmin) / width * px + a.xmin;
+			p2y = (a.ymax - a.ymin) / height * py + a.ymin;
+			a.xmin = p2x - zoom * (p2x - a.xmin);
+			a.xmax = p2x - zoom * (p2x - a.xmax);
+			a.ymin = p2y - zoom * (p2y - a.ymin);
+			a.ymax = p2y - zoom * (p2y - a.ymax);
+			mandelbrot_render(&a, frame, pitch);
 			SDL_UpdateTexture(txt, NULL, frame, pitch);
 			SDL_RenderClear(rdr);
 			SDL_RenderCopy(rdr, txt, NULL, NULL);
